4d219d7883664c11799fbb56efb897bd6ad1c236
Callum Yohn <armedjetfire23@hotmail.co.uk>
2026-01-22 17:21:21 +0000

    initialize updater.lua with repository URL and download function

--- updater.lua
+++ updater.lua
@@ -1,1 +1,21 @@
-local repo = ""
+-- updater.lua
+-- Computer Craft Script Updater
+
+local repo = "https://github.com/respiteJetfire/ccs/blob/main/"
+
+-- for any .lua file and directory in the repository, you can use the following code to download and update files.
+
+local function downloadFile(filePath)
+    local url = repo .. filePath .. "?raw=true"
+    local response = http.get(url)
+    if response then
+        local file = fs.open(filePath, "w")
+        file.write(response.readAll())
+        file.close()
+        response.close()
+        print("Updated: " .. filePath)
+    else
+        print("Failed to download: " .. filePath)
+    end
+end
+
