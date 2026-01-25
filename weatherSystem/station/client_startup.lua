e2c3e2b6e183799ae34a7bdd5e4257711e3859a8
Callum Yohn <armedjetfire23@hotmail.co.uk>
2026-01-25 16:30:42 +0000

    Update Weather Client startup script to version 1.2.0 and enable client mode in updater

--- weatherSystem/station/client_startup.lua
+++ weatherSystem/station/client_startup.lua
@@ -1,6 +1,6 @@
 -- weatherSystem/station/client_startup.lua
 -- Startup script for Weather Client (display-only, no registration)
-local version = "1.0.0"
+local version = "1.2.0"
 print("[INFO] Weather Client Startup v" .. version)
-shell.run("updater.lua")
+shell.run("updater.lua --client")
 shell.run("weatherSystem/station/client.lua")
